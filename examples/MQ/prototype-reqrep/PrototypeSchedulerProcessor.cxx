d4d382de456f7cc48eb6a2041d670fba2d1e79a3
Marcel Fiebig <marcel.fiebig@hotmail.de>
2018-08-10 12:28:45 +0200

    reduced to 20k iterations

--- examples/MQ/prototype-reqrep/PrototypeSchedulerProcessor.cxx
+++ examples/MQ/prototype-reqrep/PrototypeSchedulerProcessor.cxx
@@ -53,7 +53,7 @@ std::map<std::uint64_t, std::uint64_t> hist;
 const std::uint64_t binSize = 5;
 const double divSize = 1000; //1000 für cluster, 10000 für localhost
 
-const int iterations = 100000;
+const int iterations = 20000;
 
 struct MyMessage {
   uint64_t sendCounter;
